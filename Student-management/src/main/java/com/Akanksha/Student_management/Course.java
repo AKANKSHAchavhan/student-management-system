988c243312840a4cb402ea4fdf7a5cb7153f4cfe
Akanksha Chavhan <akankshadchavhan64@gmail.com>
2026-06-28 14:55:23 +0530

    Restore Course entity

--- Student-management/src/main/java/com/Akanksha/Student_management/Course.java
+++ Student-management/src/main/java/com/Akanksha/Student_management/Course.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-
+//temporary test
 @Entity
 public class Course {
 	@Id
